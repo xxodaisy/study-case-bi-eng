afcdb73fa77a84b9863df518096461e170c1b5d8
Fathia <main.nurfathia@gmail.com>
2026-05-19 12:36:13 +0700

    Add is_low_score flag based on review score

--- dbt_olist_analytics/staging/stg_order_reviews.sql
+++ dbt_olist_analytics/staging/stg_order_reviews.sql
@@ -10,7 +10,8 @@ renamed as(
         COALESCE(review_comment_title, 'No title provided') as review_comment_title,
         COALESCE(review_comment_message, 'No message provided') as review_comment_message,
         review_creation_date::timestamp as reviewed_at,
-        review_answer_timestamp::timestamp as review_answer_timestamp
+        review_answer_timestamp::timestamp as review_answer_timestamp,
+        case when review_score::integer <=2 then true else false end as is_low_score
     from source
 )
 
